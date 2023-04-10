b88830aa339d74d838104469e39a08b9dc5c4683
Eduardo Morais <dudu.vgvm@gmail.com>
2023-04-10 15:10:50 -0300

    Commit

---
+++ src/components/Formulario/index.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import Botao from '../Botao';
+import styles from './Formulario.module.scss'
+import { ITarefa } from '../../types/tarefas';
+import { v4 as uuidv4 } from 'uuid';
+import { useState } from 'react'
+
+interface Props {
+    setTarefas: React.Dispatch<React.SetStateAction<ITarefa[]>>
+}
+
+function Formulario({ setTarefas }: Props) {
+
+    const [tarefa, setTarefa] = useState('')
+    const [tempo, setTempo] = useState('00:00')
+
+    function adicionarTarefa(evento: React.FormEvent<HTMLFormElement>) {
+        evento.preventDefault()
+        setTarefas(
+            tarefasAntigas =>
+                [
+                    ...tarefasAntigas,
+                    {
+                        tarefa,
+                        tempo,
+                        selecionado: false,
+                        completado: false,
+                        id: uuidv4()
+                    }
+                ]
+        )
+        setTarefa('')
+        setTempo('00:00')
+    }
+
+    return (
+        <form className={styles.novaTarefa} onSubmit={adicionarTarefa}>
+            <div className={styles.inputContainer}>
+                <label>
+                    Adicione um novo estudo
+                </label>
+                <input
+                    type='text'
+                    name='tarefa'
+                    id='tarefa'
+                    value={tarefa}
+                    onChange={evento => setTarefa(evento.target.value)}
+                    placeholder='O que você quer estudar ?'
+                    required />
+            </div>
+            <div className={styles.inputContainer}>
+                <label>
+                    Tempo
+                </label>
+                <input
+                    type='time'
+                    step='1'
+                    name='tempo'
+                    value={tempo}
+                    onChange={evento => setTempo(evento.target.value)}
+                    id='tempo'
+                    min='00:00:00'
+                    max='01:30:00'
+                    required />
+            </div>
+            <Botao type='submit'>
+                Adicionar
+            </Botao>
+        </form>
+    )
+}
+
+export default Formulario
---
+++ src/components/Lista/index.tsx
@@ -0,0 +1,28 @@
+import styles from './Lista.module.scss'
+import Item from './Item';
+import { ITarefa } from '../../types/tarefas';
+
+interface Props {
+    tarefas: ITarefa[],
+    selecionaTarefa: (tarefaSelecionada: ITarefa) => void
+}
+
+function Lista({ tarefas, selecionaTarefa }: Props) {
+
+    return (
+        <aside className={styles.listaTarefas}>
+            <h2>Estudos do Dia</h2>
+            <ul>
+                {tarefas.map((item) => (
+                    <Item
+                        selecionaTarefa={selecionaTarefa}
+                        key={item.id}
+                        {...item}
+                    />
+                ))}
+            </ul>
+        </aside>
+    )
+}
+
+export default Lista;
